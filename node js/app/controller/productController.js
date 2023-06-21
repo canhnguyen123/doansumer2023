628e93f6223c39738c193a498df76a8c06e4254d
canhnguyen123 <canhnxph17083@fpt.edu.vn>
2023-06-21 21:18:21 +0700

    update

--- node js/app/controller/productController.js
+++ node js/app/controller/productController.js
@@ -16,10 +16,11 @@ exports.getAllProduct = (req, res) => {
       console.error('Lỗi truy vấn cơ sở dữ liệu: ' + error.stack);
       return res.status(500).json({ error: 'Lỗi truy vấn cơ sở dữ liệu' });
     }
-  
+
     const theloaiList = results.map((row) => {
+      const theloai_id=row.theloai_id;
       return {
-        theloai_id: row.theloai_id,
+        theloai_id: theloai_id,
         ten_theloai: arr
       };
     });
